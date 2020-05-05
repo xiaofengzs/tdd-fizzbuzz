b132c67596ff6262906d09f6c725a24b13089e4c
xiaofeng.zhi <xiaofeng.zhi@staff.smartline.com.au>
2020-05-05 20:24:34 +0800

    [xiaofeng.zhi]: refactoring

--- src/main/java/cn/xpbootcamp/fizzbuzz/Game.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/Game.java
@@ -2,25 +2,13 @@ package cn.xpbootcamp.fizzbuzz;
 
 public class Game {
     public String playGame(int number) {
-        if (number % 3 == 0) {
-            if (number % 5 == 0) {
-                if (number % 7 == 0) {
-                    return "FizzBuzzWhizz";
-                }
-                return "FizzBuzz";
-            }
-            if(number % 7 == 0) {
-                return "FizzWhizz";
-            }
-            return "Fizz";
-        } else if (number % 5 == 0) {
-            if(number % 7 == 0) {
-                return "BuzzWhizz";
-            }
-            return "Buzz";
-        } else if (number % 7 ==0) {
-            return "Whizz";
-        }
-        return "";
+        String result = "";
+        if(number % 3 == 0)
+            result += "Fizz";
+        if(number % 5 ==0)
+            result += "Buzz";
+        if(number % 7 ==0)
+            result += "Whizz";
+        return result;
     }
 }
